987748881636daa699f9b6a3be6c3645b21512be
sourav santra <souravsantra59580@gmail.com>
2020-01-01 23:26:44 +0530

    fifth commit(01/01/2019)

--- employee_panel/index.php
+++ employee_panel/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,8 +13,9 @@
     <script src="../common_files/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/index.css">
-    <script src="js/indexs.js"></script> 
+    <script src="js/indexs.js"></script>
 </head>
+
 <body>
     <div class="container-fluid">
         <div class="sidebar">
@@ -36,69 +38,124 @@
             <div class="row">
                 <div class="col-md-2"></div>
                 <div class="col-md-8 p-4 bg-white rounded-lg shadow-sm">
-                     <div>
-                         <form>
-                             <div class="from-group mb-3">
-                                 <label class="font-weight-bold" for="brand_name">Enter Brand Name</label>
-                                 <input type="text" name="brand-name" class="form-control" placeholder="ecommerce website"/>
-                             </div>
-                             <div class="from-group mb-3">
-                                 <label class="font-weight-bold" for="brand_logo">Upload brand logo</label>
-                                 <input type="file" name="brand-logo" class="form-control"/>
-                             </div>
-                             <div class="from-group mb-3">
-                                 <label class="font-weight-bold" for="domain_name">Enter domain name</label>
-                                 <input type="text" name="domain-name" class="form-control" placeholder="www.abc.com"/>
-                             </div>
-                             <div class="from-group mb-3">
-                                 <label class="font-weight-bold" for="email">Email</label>
-                                 <input type="email" name="email" class="form-control" placeholder="www.abc@gmail.com"/>
-                             </div>
-                             <div class="from-group mb-3">
-                                 <label class="font-weight-bold" for="social_handels">Social handels</label>
-                                 <input type="text" name="facebook" class="form-control mb-3" placeholder="facebook page url"/>
-                                 <input type="text" name="twitter" class="form-control" placeholder="twitter page url"/>
-                             </div>
-                             <div class="from-group mb-3">
-                                 <label class="font-weight-bold" for="address">Address</label>
-                                 <textarea class="form-control" name="address">
-
-                                 </textarea>
-                             </div>
-                             <div class="form-group mb-3">
-                                 <label class="font-weight-bold" for="phone-number">Phone</label>
-                                 <input type="number" name="phone" class="form-control" placeholder="Phone number"/>
-                             </div>
-                             <div class="from-group mb-3">
-                                 <label class="font-weight-bold" for="privacy_policy">Privacy policy</label>
-                                 <textarea class="form-control" name="privacy_policy" style="height:300px;">
-
-                                 </textarea>
-                             </div>
-                             <div class="from-group mb-3">
-                                 <label class="font-weight-bold" for="cookies_policy">Cookies policy</label>
-                                 <textarea class="form-control" name="cookies_policy" style="height:300px;">
-
-                                 </textarea>
-                             </div>
-                             <div class="from-group mb-3">
-                                 <label class="font-weight-bold" for="terms_and_condition">Terms and Condition</label>
-                                 <textarea class="form-control" name="terms_and_condition" style="height:300px;">
-
-                                 </textarea>
-                             </div>
-                            <button class="btn btn-primary">Submit Brand Information</button>
-                         </form>
-
-                     </div>
-
+                    <div>
+                        <form class="branding-form">
+                            <div class="from-group mb-3">
+                                <label class="font-weight-bold" for="brand_name">Enter Brand Name</label>
+                                <input type="text" name="brand-name" class="form-control" placeholder="ecommerce website" />
+                            </div>
+                            <div class="from-group mb-3">
+                                <label class="font-weight-bold" for="brand_logo">Upload brand logo</label>
+                                <input type="file" name="brand-logo" class="form-control" id="brand-logo" />
+                                <span style="color:red;" id="brand-logo-message"></span>
+                            </div>
+                            <div class="from-group mb-3">
+                                <label class="font-weight-bold" for="domain_name">Enter domain name</label>
+                                <input type="text" name="domain-name" class="form-control" placeholder="www.abc.com" />
+                            </div>
+                            <div class="from-group mb-3">
+                                <label class="font-weight-bold" for="email">Email</label>
+                                <input type="email" name="email" class="form-control" placeholder="www.abc@gmail.com" />
+                            </div>
+                            <div class="from-group mb-3">
+                                <label class="font-weight-bold" for="social_handels">Social handels</label>
+                                <input type="text" name="facebook" class="form-control mb-3" placeholder="facebook page url" />
+                                <input type="text" name="twitter" class="form-control" placeholder="twitter page url" />
+                            </div>
+                            <div class="from-group mb-3">
+                                <label class="font-weight-bold" for="address">Address</label>
+                                <textarea class="form-control" name="address" id="address"></textarea>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label class="font-weight-bold" for="phone-number">Phone</label>
+                                <input type="number" name="phone" class="form-control" placeholder="Phone number" />
+                            </div>
+                            <div class="from-group mb-3">
+                                <label class="font-weight-bold" for="about-us">About us <small class="about-us-count">0</small><small>/5000</small>
+                                </label>
+                                <textarea class="form-control" name="about-us" id="about-us" maxlength="5000" style="height:300px;"></textarea>
+                            </div>
+                            <div class="from-group mb-3">
+                                <label class="font-weight-bold" for="privacy-policy">Privacy policy <small class="privacy-policy-count">0</small><small>/5000</small></label>
+                                <textarea class="form-control" name="privacy-policy" id="privacy-policy" style="height:300px;" maxlength="5000"></textarea>
+                            </div>
+                            <div class="from-group mb-3">
+                                <label class="font-weight-bold" for="cookies-policy">Cookies policy <small class="cookies-policy-count">0</small><small>/5000</small></label>
+                                <textarea class="form-control" name="cookies-policy" style="height:300px;" id="cookies-policy" maxlength="5000"></textarea>
+                            </div>
+                            <div class="from-group mb-3">
+                                <label class="font-weight-bold" for="terms-and-condition">Terms and Condition <small class="terms-and-condition-count">0</small><small>/5000</small></label>
+                                <textarea class="form-control" name="terms-and-condition" style="height:300px;" id="terms-and-condition" maxlength="5000"></textarea>
+                            </div>
+                            <button class="btn btn-primary" type="submit">Submit Brand Information</button>
+                        </form>
+                    </div>
                 </div>
                 <div class="col-md-2"></div>
             </div>
-           
-
         </div>
     </div>
+    <!--start lenght count-->
+    <script>
+        $(document).ready(function() {
+            $("#about-us").on("input", function() {
+                var length = $(this).val().length;
+                $(".about-us-count").html(length);
+            });
+        });
+
+        $(document).ready(function() {
+            $("#privacy-policy").on("input", function() {
+                var length = $(this).val().length;
+                $(".privacy-policy-count").html(length);
+            });
+        });
+
+        $(document).ready(function() {
+            $("#cookies-policy").on("input", function() {
+                var length = $(this).val().length;
+                $(".cookies-policy-count").html(length);
+            });
+        });
+
+        $(document).ready(function() {
+            $("#terms-and-condition").on("input", function() {
+                var length = $(this).val().length;
+                $(".terms-and-condition-count").html(length);
+            });
+        });
+//branding form upload
+
+$(document).ready(function(){
+$(".branding-form").submit(function(e){
+    e.preventDefault();
+    var file = document.querySelector("#brand-logo");
+    var file_size = file.files[0].size;
+    if(200000>file_size)
+    {
+        $.ajax({
+         type : "POST",
+         url : "php/branding.php",
+         data : new FormData(this),
+         processData : false,
+         contentType : false,
+         cache : false,
+         success : function(response){
+             document.write(response);
+             //$(".branding-form").trigger('reset');
+         }
+
+        });
+        
+    }
+    else
+    {
+        $("#brand-logo-message").html("Please upload file less then 200kb");
+    }
+})
+});
+
+    </script>
 
 </body>
 
---
+++ employee_panel/php/branding.php
@@ -0,0 +1,62 @@
+<?php
+require_once("../../common_files/database/database.php");
+$file = $_FILES['brand-logo'];
+$location = $file['tmp_name'];
+$logo = addslashes(file_get_contents($location));
+// $string = base64_encode(file_get_contents($location));
+// $complete_url = "data:image/png;base64,".$string;
+//  echo "<img src='".$complete_url."'>";
+$brand_name = $_POST['brand-name'];
+$email = $_POST['email'];
+$domain_name = $_POST['domain-name'];
+$facebook_url = $_POST['facebook'];
+$twitter_url = $_POST['twitter'];
+$address = $_POST['address'];
+$phone = $_POST['phone'];
+$about_us = addslashes($_POST['about-us']);
+$privacy_policy = addslashes($_POST['privacy-policy']);
+$cookies_policy = addslashes($_POST['cookies-policy']);
+$terms_and_condition = addslashes($_POST['terms-and-condition']);
+
+$check_branding_table = "SELECT * FROM branding";
+$response = $db->query($check_branding_table);
+
+if ($response) {
+    $store_data = "INSERT INTO branding(brand_name,brand_logo,domain_name,email,facebook_url,twitter_url,address,phone,about_us,privacy_policy,cookies_policy,terms_and_condition)VALUES('$brand_name','$logo','$domain_name','$email','$facebook_url','$twitter_url','$address','$phone','$about_us','privacy_policy','cookies_policy','$terms_and_condition')";
+    $response = $db->query($store_data);
+    if ($response) {
+        echo "success";
+    } else {
+        echo "Failed to store data";
+    }
+} else {
+    $create_branding_table = "CREATE TABLE branding(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    brand_name VARCHAR(50),
+    brand_logo MEDIUMBLOB,
+    domain_name VARCHAR(100),
+    email VARCHAR(100),
+    facebook_url VARCHAR(255),
+    twitter_url VARCHAR(255),
+    address VARCHAR(255),
+    phone INT(12),
+    about_us MEDIUMTEXT,
+    privacy_policy MEDIUMTEXT,
+    cookies_policy MEDIUMTEXT,
+    terms_and_condition MEDIUMTEXT,
+    PRIMARY KEY(id)
+    )";
+    $response = $db->query($create_branding_table);
+    if ($response) {
+        $store_data = "INSERT INTO branding(brand_name,brand_logo,domain_name,email,facebook_url,twitter_url,address,phone,about_us,privacy_policy,cookies_policy,terms_and_condition)
+        VALUES('$brand_name','$logo','$domain_name','$email','$facebook_url','$twitter_url','$address','$phone','$about_us','privacy_policy','cookies_policy','$terms_and_condition')";
+        $response = $db->query($store_data);
+        if ($response) {
+            echo "success";
+        } else {
+            echo "Failed to store data";
+        }
+    } else {
+        echo "Unable to create table";
+    }
+}
